84cf6d707e3a53b9067931b2ef390e4e33b1d36b
EunyoungSin <ey5742@gmail.com>
2023-06-02 16:38:21 +0900

    0602

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\FlareClient\Api;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('users/{id}', );
+Route::get('users/{email}', [ApiUserController::class, 'userget']);
